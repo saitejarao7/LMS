ceacc0ae57a8c27275bab99f834323b9b188a366
saitejarao7 <saitejarao7@gmail.com>
2024-12-05 12:08:50 +0000

    web app deployed

--- script.sh
+++ script.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 echo "Deploying Web Apps on NGINX"
+
+echo "updating the system"
+sudo apt update -y
+
+echo "install utilities"
+sudo apt install -y zip unzip
+
+echo "install NGINX"
+sudo apt install -y nginx
+
+echo "remove old files"
+sudo rm -r /var/www/html
+
+echo "Deploy login app"
+sudo git clone https://github.com/saitejarao7/LMS.git /var/www/html
